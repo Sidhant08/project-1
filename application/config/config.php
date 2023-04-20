d6c2e67ee0ba6259cec523cd28e212e0413224e7
Sidhant08 <113549989+Sidhant08@users.noreply.github.com>
2023-04-20 10:22:40 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://sidhant08.github.io/project-1/';
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
